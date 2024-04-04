35ad644ccffc3713d10cd864d1e96be5f6895b7c
haeundev <haeundev@gmail.com>
2024-04-04 12:18:10 +0900

    packages

---
+++ VR Karting/Assets/EnvironmentController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherController : MonoBehaviour
+{
+    public enum WeatherType
+    {
+        MorningSunny, // Epic_BlueSunset
+        MorningSunny, // Epic_BlueSunset
+        
+    }
+    
+    
+}
