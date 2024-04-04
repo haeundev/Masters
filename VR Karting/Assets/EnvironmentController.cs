1a0c8ce6d62a144a21f8a7a760f70c5cd4082c08
haeundev <haeundev@gmail.com>
2024-04-04 12:18:30 +0900

    environment

--- VR Karting/Assets/EnvironmentController.cs
+++ VR Karting/Assets/EnvironmentController.cs
@@ -1,15 +1,72 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Rendering;
+using Random = UnityEngine.Random;
 
-public class WeatherController : MonoBehaviour
+public class EnvironmentController : MonoBehaviour
 {
+    [Serializable]
+    public class WeatherObjects : SerializedDictionary<WeatherType, GameObject>
+    {
+    }
+
+    [Serializable]
+    public class SkyboxByTimeOfDay : SerializedDictionary<TimeOfDay, Material>
+    {
+    }
+
+    [Serializable]
+    public class ColorByTimeOfDay : SerializedDictionary<TimeOfDay, Color>
+    {
+    }
+
     public enum WeatherType
     {
-        MorningSunny, // Epic_BlueSunset
-        MorningSunny, // Epic_BlueSunset
-        
+        Clear,
+        Rainy,
+        Snowy
     }
-    
-    
+
+    public enum TimeOfDay
+    {
+        Morning,
+        Afternoon,
+        Evening,
+        Night
+    }
+
+    public WeatherType weatherType;
+    public TimeOfDay timeOfDay;
+
+    public WeatherObjects weatherObjects;
+    public SkyboxByTimeOfDay skyboxByTimeOfDay;
+    public ColorByTimeOfDay colorByTimeOfDay;
+
+    [SerializeField] private Light directionalLight;
+
+    [Button]
+    public void SetRandomEnvironment()
+    {
+        var randomWeather = (WeatherType)Random.Range(0, Enum.GetValues(typeof(WeatherType)).Length);
+        var randomTime = (TimeOfDay)Random.Range(0, Enum.GetValues(typeof(TimeOfDay)).Length);
+
+        SetEnvironment(randomWeather, randomTime);
+    }
+
+    public void SetEnvironment(WeatherType weather, TimeOfDay time)
+    {
+        weatherType = weather;
+        timeOfDay = time;
+
+        if (weatherType != WeatherType.Clear)
+            foreach (var obj in weatherObjects)
+                obj.Value.SetActive(obj.Key == weatherType);
+        else
+            foreach (var obj in weatherObjects)
+                obj.Value.SetActive(false);
+
+        RenderSettings.skybox = skyboxByTimeOfDay[timeOfDay];
+        directionalLight.color = colorByTimeOfDay[timeOfDay];
+    }
 }
